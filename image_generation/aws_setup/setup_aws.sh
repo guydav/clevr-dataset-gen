2ba20f9af1a720a7a179b20388ac1da9156aa157
Guy Davidson <guydav@gmail.com>
2019-01-20 19:26:07 -0800

    Added bc installation and psutil copying to AWS setup script

--- image_generation/aws_setup/setup_aws.sh
+++ image_generation/aws_setup/setup_aws.sh
@@ -7,12 +7,14 @@ sudo add-apt-repository -y ppa:thomas-schiex/blender-legacy
 sudo apt-get install -y nvidia-modprobe
 sudo apt update -y
 sudo apt-get install -y blender
+sudo apt-get install -y bc
 
 # Add startup script
 sudo python ./add_blender_startup_script.py
 
-# Fix the built-in OS numpy
+# Fix the built-in OS numpy, add psutil
 sudo cp -R /home/ubuntu/anaconda3/envs/python3/lib/python3.6/site-packages/numpy /usr/lib/python3/dist-packages
+sudo cp -R /home/ubuntu/anaconda3/envs/python3/lib/python3.6/site-packages/psutil /usr/lib/python3/dist-packages
 
 # Fix a small thing in Blender's CUDA scripts
 sudo python ./fix_blender_cuda.py
